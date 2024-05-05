e0441d7ad5f8e7cd447aa4bbef0376e77578900b
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-05-05 16:44:39 +0530

    Header added

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit, distinctio minus fugit tempora soluta quam fuga vitae corporis enim explicabo aliquid vero laboriosam sint atque repudiandae in! Reiciendis, quaerat?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
