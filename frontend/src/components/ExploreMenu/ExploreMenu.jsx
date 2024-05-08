a2a9417188a199e761096f362a1d637681aa3464
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-05-08 10:12:15 +0530

    useState of Category added, values passed through props home to explore-menu, active class were added to menu image

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       
       <h1>Explore our menu</h1>
       <p className='explore-menu-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, quod ducimus consequatur nihil inventore natus sint eligendi aut corrupti reiciendis odio nobis fuga accusantium veritatis tempora pariatur, temporibus nisi incidunt.</p>
       
+
       <div className="explore-menu-list">
+
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} alt='' />
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+              
+              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt='' />
               <p>{item.menu_name}</p>
+
             </div>
           )
         })}
+
       </div>
       <hr/>
     </div>
