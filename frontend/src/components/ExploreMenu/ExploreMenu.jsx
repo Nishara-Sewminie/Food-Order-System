161ee86951c0b12aafaa06603edc436cb4c85b58
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-05-08 09:17:12 +0530

    Ëxplore-menu styles were added

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,18 +5,21 @@ import { menu_list } from '../../assets/assets'
 const ExploreMenu = () => {
   return (
     <div className='explore-menu' id='explore-menu'>
-<h1>Explore our menu</h1>
-<p className='explore-menu-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, quod ducimus consequatur nihil inventore natus sint eligendi aut corrupti reiciendis odio nobis fuga accusantium veritatis tempora pariatur, temporibus nisi incidunt.</p>
-<div className="explore-menu-list">
-  {menu_list.map((item,index)=>{
-    return (
-      <div key={index} className="explore-menu-list-item">
-        <img src={item.menu_image} alt='' />
-        <p>{item.menu_name}</p>
+      
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, quod ducimus consequatur nihil inventore natus sint eligendi aut corrupti reiciendis odio nobis fuga accusantium veritatis tempora pariatur, temporibus nisi incidunt.</p>
+      
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="explore-menu-list-item">
+              <img src={item.menu_image} alt='' />
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
       </div>
-    )
-  })}
-</div>
+      <hr/>
     </div>
   )
 }
