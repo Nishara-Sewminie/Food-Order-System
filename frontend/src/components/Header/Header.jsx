c9e4c1be11ed11055b0c5c2cfb9aa75d732720ea
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-05-11 08:48:04 +0530

    v

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,11 +4,12 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit, distinctio minus fugit tempora soluta quam fuga vitae corporis enim explicabo aliquid vero laboriosam sint atque repudiandae in! Reiciendis, quaerat?</p>
-            <button>View Menu</button>
-        </div>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit, distinctio minus fugit tempora soluta quam fuga vitae corporis enim explicabo aliquid vero laboriosam sint atque repudiandae in! Reiciendis, quaerat?</p>
+        <button>View Menu</button>
+      </div>
+
     </div>
   )
 }
